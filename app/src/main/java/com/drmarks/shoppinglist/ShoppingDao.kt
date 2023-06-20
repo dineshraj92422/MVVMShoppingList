634b46683b45dfcadc503ce9513b3aa0bca90e1d
Dineshsingh Rajpurohit <dineshsinghrajpurohit@Dineshsinghs-MacBook-Air.local>
2023-06-21 00:54:18 +0530

    Initial Project

---
+++ app/src/main/java/com/drmarks/shoppinglist/ShoppingDao.kt
@@ -0,0 +1,20 @@
+package com.drmarks.shoppinglist
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface ShoppingDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun upsert(item: ShoppingItem)         // It is combination of insert and update. If a data available in DB it will update it otherwise insert it.
+
+    @Delete
+    suspend fun delete(item: ShoppingItem)   //suspend is to call function asynchronously
+
+    @Query("SELECT * FROM shopping_items")
+    fun getAllShoppingItems(): LiveData<List<ShoppingItem>>
+}
